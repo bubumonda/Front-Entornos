3194cb2ebc684e57b574ef0c358b2fa159abfef8
Gian Mier <gian2210073@correo.uis.edu.co>
2023-11-21 15:00:04 -0500

    Proyecto front

--- src/app/components/home/components/layout/layout.component.ts
+++ src/app/components/home/components/layout/layout.component.ts
@@ -22,6 +22,10 @@ export class LayoutComponent implements OnInit, AfterViewChecked {
   ngOnInit(): void {
   }
 
+  onLogOut(){
+    localStorage.clear();
+    this.router.navigateByUrl('/auth/login');
+  }
 
 
 }
